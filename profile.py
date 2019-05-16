8529969e9bc24788ea3d9145bb189e3d3babd7ff
pmackevicius <50683125+pmackevicius@users.noreply.github.com>
2019-05-15 20:57:47 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -21,50 +21,52 @@ request.addTour(tour)
 
 prefixForIP = "192.168.1."
 
-for i in range(3):
-  if i == 0:
-    node = request.RawPC("KVM")
-    node.hardware_type = "c220g2"
-    node.routable_control_ip = "true"
-    node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU16-64-STD"
-    iface = node.addInterface("if" + str(i))
-    iface.component_id = "eth1"
-    iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
-    link.addInterface(iface)
-    
-    
-    #node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/setup_kvm.sh"))
-    
-  elif i == 1:
-    node = request.RawPC("Docker")
-    node.hardware_type = "c220g2"
-    node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU16-64-STD"
-    node.routable_control_ip = "true"
-    
-    iface = node.addInterface("if" + str(i))
-    iface.component_id = "eth1"
-    iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
-    link.addInterface(iface)
-    
-    
-    node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/setup_docker.sh"))
-    node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/linpack/docker_linpack.sh"))
-  
-  elif i == 2:
-    node = request.RawPC("Singularity")
-    node.hardware_type = "c220g2"
-    node.routable_control_ip = "true"
-    
-    node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU16-64-STD"
-    iface = node.addInterface("if" + str(i))
-    iface.component_id = "eth1"
-    iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
-    link.addInterface(iface)
-    
-    
-    node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/setup_singularity.sh"))
-    node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/linpack/singularity_linpack.sh"))
+# Setup KVM node
 
+node1 = request.RawPC("KVM")
+node1.hardware_type = "c220g2"
+node1.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU16-64-STD"
+
+iface1 = node1.addInterface("if" + str(1))
+iface1.component_id = "eth1"
+iface1.addAddress(pg.IPv4Address(prefixForIP + str(1), "255.255.255.0"))
+
+    
+#node1.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/setup_kvm.sh"))
+
+# Setup Docker node
+
+node2 = request.RawPC("Docker")
+node2.hardware_type = "c220g2"
+node2.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU16-64-STD"
+
+    
+iface2 = node2.addInterface("if" + str(2))
+iface2.component_id = "eth2"
+iface2.addAddress(pg.IPv4Address(prefixForIP + str(2), "255.255.255.0"))
+
+       
+node2.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/setup_docker.sh"))
+node2.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/linpack/docker_linpack.sh"))
+
+# Setup Singularity node
+
+node3 = request.RawPC("Singularity")
+node3.hardware_type = "c220g2"
+node3.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU16-64-STD"
+
+    
+iface3 = node3.addInterface("if" + str(3))
+iface3.component_id = "eth3"
+iface3.addAddress(pg.IPv4Address(prefixForIP + str(3), "255.255.255.0"))
+
+       
+node3.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/setup_singularity.sh"))
+node3.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/linpack/singularity_linpack.sh"))
+
+link.addInterface(iface1)
+link.addInterface(iface2)
+link.addInterface(iface3)
     
   
 pc.printRequestRSpec(request)
