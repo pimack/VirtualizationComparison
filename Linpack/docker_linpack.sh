5dafb5a2178910b651c246359fc96c2f7afb61de
pmackevicius <50683125+pmackevicius@users.noreply.github.com>
2019-05-15 20:24:03 -0400

    Delete docker_linpack.sh

--- Linpack/docker_linpack.sh
+++
@@ -1,7 +0,0 @@
-set -x
-
-sudo git clone https://github.com/pmackevicius/VirtualizationComparison
-cd VirtualizationComparison/linpack
-sudo wget http://registrationcenter-download.intel.com/akdlm/irc_nas/2169/l_lpk_p_10.3.4.007.tgz
-sudo tar zxvf l_lpk_p_10.3.4.007.tgz
-sudo docker build -t linpack .
