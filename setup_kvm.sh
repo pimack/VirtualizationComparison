ad8ef24d7d28ed926d8e587d6a337085ee3cd698
pimack <50683125+pimack@users.noreply.github.com>
2019-05-22 01:29:56 -0400

    Update setup_kvm.sh

--- setup_kvm.sh
+++ setup_kvm.sh
@@ -2,18 +2,19 @@ set -x
 
 sudo apt-get update
 sudo apt-get install -y qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils virt-manager libosinfo-bin libguestfs-tools virt-top
-sudo adduser `id -un` libvirtd
-sudo wget http://releases.ubuntu.com/16.04/ubuntu-16.04.6-server-amd64.iso
+sudo adduser $USER libvirtd
+sudo wget http://centos.mirrors.hoobly.com/7.6.1810/isos/x86_64/CentOS-7-x86_64-Minimal-1810.iso
 sudo qemu-img create -f qcow2 ubuntu.qcow2 20G
-sudo git clone https://github.com/pmackevicius/VirtualizationComparison
+sudo git clone https://github.com/pimack/VirtualizationComparison
 sudo virt-install --name test \
 --connect qemu:///system \
 --ram 16384 \
 --disk path=ubuntu.qcow2,format=qcow2,bus=virtio,size=8 \
---vcpus 4 \
---os-type linux \
+--vcpus 8 \
+--os-variant=centos7.0 \
 --network network=default \
 --graphics none \
 --console pty,target_type=serial \
---location=ubuntu-16.04.6-server-amd64.iso \
---extra-args 'console=ttyS0'
+--location=CentOS-7-x86_64-Minimal-1810.iso \
+--initrd-inject VirtualizationComparison/Kickstarter.cfg \
+--extra-args 'ks=file:/Kickstarter.cfg console=tty0 console=ttyS0' 
